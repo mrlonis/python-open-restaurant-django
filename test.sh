c5141ab5cfb0d505e927dd51590862c1151fdd6d
Matthew Lonis <mattlonis@gmail.com>
2024-02-12 00:49:14 -0800

    Updating dependencies

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker compose up --build --pull postgresql --remove-orphans -V --wait
+docker compose up --build --pull always --remove-orphans -V --wait
 poetry run python manage.py migrate
 poetry run python manage.py test
